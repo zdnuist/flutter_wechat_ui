730af06b43840d339f9db5512eb4b8488765c669
zhangdong <zhangdong123@loocha.com>
2018-06-11 15:32:58 +0800

    add find page

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../test/demo1.dart';
 
 class ChatPage extends StatefulWidget {
 
--- lib/pages/find.dart
+++ lib/pages/find.dart
@@ -8,14 +8,103 @@ class FindPage extends StatefulWidget {
     }
 }
 
+class Item{
+   Icon icon;
+   int type;
+   String title;
+
+  Item(this.icon,this.title,this.type);
+}
+
 class FindPageState extends State<FindPage> {
+
+  List<Item> items;
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      loadDtas();
+    }
+
+  void loadDtas(){
+    items = <Item>[
+
+      new Item(new Icon(Icons.cloud_circle) , "朋友圈", 0),
+      new Item(null , "", 1),
+
+      new Item(new Icon(Icons.scanner) , "扫一扫", 0),
+      new Item(new Icon(Icons.healing) , "摇一摇", 0),
+      new Item(null , "", 1),
+
+      new Item(new Icon(Icons.looks) , "看一看", 0),
+      new Item(new Icon(Icons.search) , "搜一搜", 0),
+      new Item(null , "", 1),
+
+      new Item(new Icon(Icons.person) , "附近的人", 0),
+      new Item(new Icon(Icons.border_outer) , "漂流瓶", 0),
+      new Item(null , "", 1),
+
+      new Item(new Icon(Icons.shop) , "购物", 0),
+      new Item(new Icon(Icons.games) , "游戏", 0),
+      new Item(null , "", 1),
+
+      new Item(new Icon(Icons.link) , "小程序", 0),
+
+
+    ];  
+  }
 
   @override
     Widget build(BuildContext context) {
       // TODO: implement build
-      return  new Text("find");
+      return new Container(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: new ListView.builder(
+        
+        itemBuilder: (context, i){
+          if(i.isOdd) return new Divider(height: 1.0,);
+          int index = i~/2;
+          if(index >= items.length){
+            return null;
+          }
+          return buildRow(items[index]);
+        },
+      ),
+      color: const Color(0xFFf0eff5),
+      );
     }
 
+
+    Widget buildRow(Item item){
+      print(item.title);
+
+      if(item.type == 0){
+        return new Container(
+          color: Colors.white,
+          height: 44.0,
+          padding: const EdgeInsets.only(left: 10.0,right: 10.0),
+          child: new Row(
+            children: <Widget>[
+              item.icon,
+              new Expanded(
+                child: new Container(
+                  child:Text(item.title) ,
+                  padding: const EdgeInsets.only(left: 15.0),
+                ) ,
+              ),
+              new Icon(Icons.arrow_right)
+            ],
+          ),
+        );
+      }
+
+      return new Container(
+        height: 23.0,
+      );
+
+
+    }
     
 
 
